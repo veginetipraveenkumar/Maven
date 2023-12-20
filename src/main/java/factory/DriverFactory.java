4b51c03bcc0817a3c9a81285bc42f0962fe3b268
praveenautomation <praveenkumar@gmail.com>
2023-12-21 02:03:32 +0530

    firstcommit

---
+++ src/main/java/factory/DriverFactory.java
@@ -0,0 +1,37 @@
+package factory;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.bouncycastle.oer.Switch;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverFactory {
+    public static WebDriver initializeDriver(String browsers) {
+        WebDriver driver = null;
+        switch (browsers) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--start-maximize");
+                options.addArguments("--remote-allow-origins=*");
+                options.addArguments("--disable-notifications");
+                options.addArguments("--incognito");
+                driver = new ChromeDriver(options);
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+            default:
+                System.out.println("invalid browser name,give a valid name");
+        }
+        return driver;
+    }
+}
---
+++ src/test/java/sampleTest/FirstTest.java
@@ -0,0 +1,15 @@
+package sampleTest;
+
+import factory.DriverFactory;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstTest {
+    public static void main(String args[]) {
+        WebDriver driver= DriverFactory.initializeDriver("edge");
+        driver.get("https://www.automationzone.in");
+        driver.quit();
+    }
+}
+
